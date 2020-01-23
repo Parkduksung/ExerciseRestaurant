8e9e2b7c76ecd1ec082d7e726f4440fb3654145d
Parkduksung <duksung1234@naver.com>
2020-01-23 15:20:12 +0900

    injection 파일 위치이동

---
+++ app/src/main/java/com/work/restaurant/Injection.kt
@@ -0,0 +1,21 @@
+package com.work.restaurant
+
+import com.work.restaurant.data.repository.eat.EatRepository
+import com.work.restaurant.data.repository.eat.EatRepositoryImpl
+import com.work.restaurant.data.source.local.eat.EatLocalDataSourceImpl
+import com.work.restaurant.network.room.database.EatDatabase
+import com.work.restaurant.util.App
+import com.work.restaurant.util.AppExecutors
+
+
+object Injection {
+    fun provideEatRepository(): EatRepository =
+        EatRepositoryImpl.getInstance(
+            EatLocalDataSourceImpl(
+                AppExecutors(), EatDatabase.getInstance(
+                    App.instance.context()
+                )
+            )
+        )
+
+}
